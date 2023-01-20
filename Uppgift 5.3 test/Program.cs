80432cc5745441259dfe6042f91719889224cbff
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:25:44 +0100

    Add project files.

---
+++ Uppgift 5.3 test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 3 heltal");
+            int tal = int.Parse(Console.ReadLine());
+            string[] taler = new string[tal];
+
+            for (int i = 0; i < tal; i++)
+            {
+                Console.WriteLine("Skriv in dina heltal");
+                taler[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Här är de talen du skrev in");
+            for (int i = 0; i < tal; i++)
+            {
+                Console.WriteLine(taler[i]);
+            }
+        }
+    }
+}
